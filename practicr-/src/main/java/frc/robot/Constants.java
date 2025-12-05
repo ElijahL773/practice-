e9b1a0b07ad13f0592d725ab76e6795f0d5fc6a5
ElijahL773 <elijahr2@vandenrobotics.com>
2025-12-04 16:55:19 -0800

    Added motors

--- practicr-/src/main/java/frc/robot/Constants.java
+++ practicr-/src/main/java/frc/robot/Constants.java
@@ -16,5 +16,10 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class IntakeConstants{
+    public static final int kIntakeMotor1 = 1;
+    public static final int kIntakeMotor2 = 2;
+  }
+ 
   
 }
